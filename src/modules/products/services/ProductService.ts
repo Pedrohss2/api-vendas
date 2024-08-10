2a4c2242882e70920aba110f05ddc2d46c05fcd8
PedroH <pedro2266809ph@gmail.com>
2024-08-09 21:33:06 -0300

    feat: Add classe de service CreateProductService

---
+++ src/modules/products/services/ProductService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+import AppError from "@shared/errors/appError";
+import Product from "../typeorm/entities/Product";
+import { promises } from "dns";
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  
+  public async create ({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+    const productExist = await productsRepository.findByName(name);
+    
+    if (productExist) {
+      throw new AppError("Product exist!");
+    } 
+    
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+    return product;
+  }
+}
+
+export default CreateProductService;
