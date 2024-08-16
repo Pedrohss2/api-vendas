2783b55243b723b7e5fa1f8982d2f75c3a952b64
PedroH <pedro2266809ph@gmail.com>
2024-08-15 21:51:33 -0300

    feat: migration de usuarios

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors'
 import cors from 'cors';
+import { errors } from 'celebrate'
 import routes from './routes'
 import AppError from '@shared/errors/appError';
 import '@shared/typeorm';
+import { error } from 'console';
 
 const app = express();
 
@@ -12,6 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
+
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
 
   if (error instanceof AppError) {
