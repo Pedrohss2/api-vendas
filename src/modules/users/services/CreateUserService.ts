2a382ab7585c488fc0505fde1dc61d04e0eb685e
PedroH <pedro2266809ph@gmail.com>
2024-08-17 14:52:55 -0300

    feat: implementando hash do password

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,7 +2,8 @@ import AppError from "@shared/errors/appError";
 import User from "../typeorm/entities/User";
 import UserRepository from "../typeorm/repositories/UserRepository";
 import { getCustomRepository } from "typeorm";
-import e from "express";
+import express from "express";
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -15,11 +16,12 @@ class CreateUserService {
   public async create({ name, email, password }: IRequest): Promise<User | AppError> {
     const userRepository = getCustomRepository(UserRepository);
     const emailExist = await userRepository.findByEmail(email);
-    
+
+    const hashedPassword = await hash(password, 8);
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
