08fd591c168343f89bf793ce2aa28395b9e7c89c
PedroH <pedro2266809ph@gmail.com>
2024-08-17 12:27:24 -0300

    feat: rotas de user

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -23,7 +23,7 @@ productsRouter.post(
     [Segments.BODY]: {
       name: Joi.string().required(),
       price: Joi.number().precision(2).required(),
-      qunatity: Joi.number().required(),
+      quantity: Joi.number().required(),
     }
   }),
   productsController.create
@@ -35,7 +35,7 @@ productsRouter.put(
     [Segments.BODY]: {
       name: Joi.string().required(),
       price: Joi.number().precision(2).required(),
-      qunatity: Joi.number().required(),
+      quantity: Joi.number().required(),
     }, 
     [Segments.PARAMS]: {
       id: Joi.string().uuid().required(),
