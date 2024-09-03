6ee7e9f9eb73c2f74ed25463a35f6538a9d4942c
PedroH <pedro2266809ph@gmail.com>
2024-09-03 15:21:18 -0300

    feat: criando camada de serviço,controller para  gerenciamento de senha

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,41 @@
+import AppError from "@shared/errors/appError";
+import { getCustomRepository } from "typeorm";
+import { isAfter, addHours } from 'date-fns';
+import UserRepository from "../typeorm/repositories/UserRepository";
+import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import { hash } from "bcryptjs";
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResePasswordService {
+
+  public async create({ token, password }: IRequest): Promise<void> {
+    const userRepository = getCustomRepository(UserRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) throw new AppError('User Token does not exists!');
+
+    const user = await userRepository.findById(userToken.user_id);
+
+    if (!user) throw new AppError('User does not exists!');
+    
+    this.validateHours(userToken.created_at);
+    
+    user.password = await hash(password, 8);
+  }
+
+  public async validateHours(created_at: Date): Promise<void> {
+    const tokenCreatedAt = created_at;
+
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    isAfter(Date.now(), compareDate) ? new AppError('Token exprired') : ''; 
+  }
+}
+
+export default ResePasswordService;
