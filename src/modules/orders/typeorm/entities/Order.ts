807a31b75c86fcda73a767a6a62de49046eb7ec1
PedroH <pedro2266809ph@gmail.com>
2024-09-11 14:30:45 -0300

    feat: entidade de order

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,20 @@
+import Customers from "@modules/customers/typeorm/entities/Customers";
+import {CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customers)
+  @JoinColumn({ name: 'customer_id'}) 
+  custumer: Customers;
+    
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Order;
